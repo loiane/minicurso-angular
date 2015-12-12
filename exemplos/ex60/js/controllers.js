b18b9fc199e80b9f5a62809ae07c8f6e0e09a225
Loiane Groner <loianeg@gmail.com>
2015-12-12 12:04:10 -0200

    exemplo 60

---
+++ exemplos/ex60/js/controllers.js
@@ -0,0 +1,53 @@
+angular.module('todoController', [])
+
+    .controller('tasksController', ['$scope', 'TasksService', function($scope, TasksService) {
+
+        $scope.tasks = TasksService.get(); //[{text: 'Teste1'}, {text: 'Teste2'}];
+
+        $scope.formData = {};
+
+        $scope.createTodo = function() {
+
+            if ($scope.formData.text != undefined) {
+
+                var newTask = {
+                    id: TasksService.getNextId(),
+                    text: $scope.formData.text
+                };
+
+                $scope.tasks.push(newTask);
+
+                //console.log($scope.tasks);
+                TasksService.save($scope.tasks);
+
+                $scope.formData = {};
+            }
+        };
+
+        var editTask = function(taskName, task){
+
+            for(var i=0; i<$scope.tasks.length; i++){
+                if ($scope.tasks[i].id === task.id){
+                    $scope.tasks[i].text = taskName;
+                    break;
+                }
+            }
+            TasksService.save($scope.tasks);
+        };
+
+        $scope.editTodo = function(task) {
+
+            var taskName = prompt("Atualize a tarefa:", task.text);
+            if (taskName !== null) {
+                editTask(taskName, task);
+            }
+        };
+
+        $scope.deleteTodo = function(task) {
+
+            $scope.tasks.splice($scope.tasks.indexOf(task), 1);
+            TasksService.save($scope.tasks);
+
+        };
+
+    }]);
